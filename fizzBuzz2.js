2b04ea2169090d23012fc718751998f67749816b
Aaron Long <aaronlng05@gmail.com>
2020-03-25 17:10:51 -0400

    solved part 1 and part 2

--- fizzBuzz2.js
+++ fizzBuzz2.js
@@ -1,6 +1,8 @@
-console.log("------------------Part 1-------------------------");
+console.log("------------------Part 2-------------------------");
 for (let num = 1; num <= 100; num++) {
-    if (num % 3 == 0){
+    if ((num % 3 == 0) && (num % 5 == 0)){
+        console.log("FizzBuzz");
+    } else if (num % 3 == 0){
         console.log("Fizz");
     } else if (num % 5 == 0){
         console.log("Buzz");
